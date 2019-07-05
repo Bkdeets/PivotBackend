c4e3e02aa346772cbfe30952d650c953856ddb8d
Britton <brittonkdeets@gmail.com>
2019-07-05 19:46:13 -0400

    adding some models

--- autotrade/models.py
+++ autotrade/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Django User model
+
+class Profile(models.Model):
+    bio = models.CharField(max_length=200, null=True)
+
+
+class Strategy(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    isProfitable = models.BooleanField()
